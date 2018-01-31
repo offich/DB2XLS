dcd75ae1770659b39cd0337dfcf84af5dc45ac03
r-ume <baymax.blackandwhite@gmail.com>
2018-01-31 14:38:59 +0900

    added main_script.rb

---
+++ main_script.rb
@@ -0,0 +1,16 @@
+require './csv_file_creator.rb'
+require './db_client.rb'
+
+p 'こちらはDB2CSVレポジトリです。DBのスキーマをCSVに吐き出します。'
+
+file_name = ARGV[0]
+
+if file_name.nil?
+  p 'ファイル名の入力をお願いします。'
+  return
+end
+
+data = DbClient.instance.columns_in_all_tables
+CsvFileCreator.new(file_name: "./#{file_name}.csv").generate_file(data: data)
+
+p 'CSVへの吐き出しが終了しました'
