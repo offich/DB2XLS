8cf83314180ba25ff0bbf2b92a98d0a0b3f63edc
r-ume <baymax.blackandwhite@gmail.com>
2018-02-02 13:25:30 +0900

    defined new_sub_sheet, sub_sheet_name, sub_sheet_header, sub_sheet_cells, save_sub_sheet methods

--- app/v2/outs/xls.rb
+++ app/v2/outs/xls.rb
@@ -1,5 +1,6 @@
-require 'rubygems'
-require 'spreadsheet'
+require          'rubygems'
+require          'spreadsheet'
+require_relative '../interfaces/schema_formatter'
 
 module App
   module V2
@@ -9,12 +10,46 @@ module App
 
         attr_accessor :file_name
 
+        HEADER_ROW = 0
+
         def initialize(file_name:)
           @file_name = file_name
+          @sheet     = Spreadsheet::Workbook.new
         end 
 
-        def format(schema)
+        def format(schemas:)
+          schemas.each do |schema|
+            self.new_sub_sheet
+            self.sub_sheet_name(name: schema.table_name)
+            self.sub_sheet_header(header: schema.header)
+            self.sub_sheet_cells(columns: schema.columns)
+            self.save_sub_sheet
+          end
+        end
 
+        def new_sub_sheet
+          @sub_sheet = @sheet.create_worksheet
+        end
+
+        def sub_sheet_name(name:)
+          @sub_sheet.name = name
+        end
+
+        def sub_sheet_header(header:)
+          @sub_sheet.row(0).concat(header)
+        end
+
+        def sub_sheet_cells(columns:)
+          columns.each_with_index do |cell_row, cell_column|
+            cell_row.each_with_index do |cell, cell_index_in_row|
+              cell_column += 1 if !@sub_sheet[cell_column, cell_index_in_row].nil?
+              @sub_sheet[cell_column, cell_index_in_row] = cell
+            end
+          end
+        end
+
+        def save_sub_sheet
+          @sheet.write(@file_name)
         end
       end
     end
